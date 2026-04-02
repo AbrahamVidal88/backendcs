5faf21ff214115b1ebd7f010cf8fe6216ea0071f
AbrahamVidal88 <abraham.vidal.pat@gmail.com>
2026-04-01 23:57:22 -0500

    Proyecto api

---
+++ Backend/Controllers/PeopleController.cs
@@ -0,0 +1,82 @@
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+        private IPeopleService _peopleService;
+
+        public PeopleController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+
+        [HttpGet("{id}")]
+        public ActionResult<People> Get(int id)
+        {
+           var people =  Repository.People.FirstOrDefault(p => p.Id == id);
+
+            if (people == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(people);
+        }
+        [HttpGet("search/{search}")]
+        public List<People> Get(string search) =>
+            Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+
+        [HttpPost]
+        public IActionResult Add(People people)
+        {
+            if(!_peopleService.Validate(people))
+            {
+                return BadRequest();
+            }
+
+            Repository.People.Add(people);
+
+            return NoContent();
+        }
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People>
+        {
+            new People()
+            {
+                Id= 1, Name = "Pedro", Birtdate = new DateTime(1999, 12, 3)
+            },
+            new People()
+            {
+                Id= 2, Name = "Juan", Birtdate = new DateTime(1988, 2, 18)
+            },
+            new People()
+            {
+                Id= 3, Name = "Anderson", Birtdate = new DateTime(2000, 4, 4)
+            },
+            new People()
+            {
+                Id= 4, Name = "Lorenzo", Birtdate = new DateTime(1975, 8, 20)
+            }
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime Birtdate {  get; set; }
+    }
+}
---
+++ Backend/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Backend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        IPostsService _titleService;
+
+        public PostsController(IPostsService titleService)
+        {
+            _titleService = titleService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PostDto>> Get() =>
+            await _titleService.Get();
+    }
+}
