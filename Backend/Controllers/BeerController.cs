9ead54b9e6278f52b04220217ef72c0feee768b1
Euronymouskar <abraham.vidal.pat@gmail.com>
2026-05-21 00:13:15 -0500

    avances de curso

---
+++ Backend/Controllers/BeerController.cs
@@ -0,0 +1,31 @@
+﻿using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private StoreContext _context;
+
+        public BeerController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<BeerDto>> Get() =>
+            await _context.Beers.Select(b => new BeerDto
+            {
+                Id = b.BeerId,
+                Name = b.Name,
+                BrandId = b.BrandId,
+                Alcohol = b.Alcohol
+            }).ToListAsync();
+
+    }
+}
