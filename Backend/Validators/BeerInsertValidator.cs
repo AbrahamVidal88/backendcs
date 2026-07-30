35b3cc4998210009ea9a81bc344ad076f41aa405
AbrahamVidal88 <abraham.vidal.pat@gmail.com>
2026-07-30 12:51:58 -0500

    agregando mensajes de validacion en BeerInsertValidator para mostrarlos en las respuestas del json

--- Backend/Validators/BeerInsertValidator.cs
+++ Backend/Validators/BeerInsertValidator.cs
@@ -7,7 +7,11 @@ namespace Backend.Validators
     {
         public BeerInsertValidator()
         {
-            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().WithMessage("El nombre es obligatorio");
+            RuleFor(x => x.Name).Length(2, 20).WithMessage("El nombre debe medir entre 2 a 20 caracteres");
+            RuleFor(x => x.BrandId).NotNull().WithMessage(x => "Error con el valor enviado de marca");
+            RuleFor(x => x.BrandId).GreaterThan(0).WithMessage(x => "Error con el valor enviado de marca");
+            RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage(x => "El {PropertyName} debe ser mayor a 0");
         }
     }
 }
