8b539f8cb626bf8c9c9a2ec53dc2ec4b216afecb
AbrahamVidal88 <abraham.vidal.pat@gmail.com>
2026-07-30 12:35:44 -0500

    carpeta Validators y clase para validar inserciones de Beer creada

---
+++ Backend/Validators/BeerInsertValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Backend.DTOs;
+
+namespace Backend.Validators
+{
+    public class BeerInsertValidator : AbstractValidator<BeerInsertDto>
+    {
+        public BeerInsertValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
